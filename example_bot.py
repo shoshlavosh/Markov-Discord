3715f555b0361baae9952d7bb2586fb22e4954ad
shoshlavosh <shoshlavosh@gmail.com>
2021-07-21 20:00:11 +0000

    Added example_bot.py

---
+++ example_bot.py
@@ -0,0 +1,19 @@
+import os
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('replace this with secrets.sh')
